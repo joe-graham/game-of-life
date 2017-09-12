b521ecd42300162e88f6d934ce34e7a97daf5837
Joe Graham <jmg3484@rit.edu>
2017-09-12 10:40:02 -0400

    game board init

--- game-of-life.py
+++ game-of-life.py
@@ -1,7 +1,11 @@
 # game-of-life.py - An implementation of Conway's Game of Life in Python.
 # Joe Graham - 9/11/17
 
-import argparse
+import argparse, io
+
+# Parse in config filename, number of generations, and whether or not to pause
+# between generations from the command line. argparse will auto stop if not
+# enough mandatory parameters are passed as arguments.
 
 parser = argparse.ArgumentParser(description="Conway's Game of Life in Python.")
 parser.add_argument("config", help="Configuration file " +
@@ -11,5 +15,34 @@ parser.add_argument("gens", metavar="generations",
 parser.add_argument("--continuous", "-c", action="store_true",
     help="Run the simulation continuously without stopping for each generation.")
 
+# Parse arguments and assign them to variables.
+
 args = parser.parse_args()
-print args
+configFilename = args.config
+numGens = args.gens
+continuousFlag = args.continuous
+
+# Read in the config file.
+
+config = io.open(configFilename, mode='r')
+firstLine = True
+for line in config:
+    # Ignore comments.
+    if not line.startswith("#"):
+        # Initialize the board if this is the first line of the file.
+        if firstLine:
+            numRows = int(line[0])
+            numCols = int(line[2])
+            firstLine = False
+            gameBoard = [[0]*numCols for _ in range(numRows)]
+            curRow = 0
+            curCol = 0
+        # Read in the current row if this isn't the first line.
+        else:
+            for char in line.rstrip():
+                if str(char) != "0":
+                    gameBoard[curRow][curCol] = 1
+                curCol += 1
+            curRow += 1
+            curCol = 0
+print gameBoard
